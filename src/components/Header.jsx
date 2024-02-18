861d4b4a95c53117a3d6910b298d4c769864aa13
Dario DG <dario.droid.z2@gmail.com>
2024-02-18 15:27:11 +0000

    Second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sm:mt-40 sm:mb-16 my-10">
+    <header className="sm:mb-16 my-10">
       <h1 className=" text-2xl uppercase flex flex-col justify-center items-center text-darkGrayishCyan tracking-widest ">
         <span>Spli</span>
         <span>tter</span>
